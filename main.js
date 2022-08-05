c6befe251919ebdbae53efd41407f51508661316
Mariana <marianaceciliaschmidt@gmail.com>
2022-08-05 14:19:42 -0300

    initial board created

---
+++ main.js
@@ -0,0 +1,27 @@
+//initialize the game board on page load
+initBoard()
+
+function initBoard() {
+    let board = document.getElementById('clue-board')
+
+    //generate 5 rows, then place 6 boxes in each row
+    for(let i = 0; i < 5; i++){
+        let row = document.createElement('div')
+        let boxValue = 200*(i + 1)
+        row.className = 'clue-row'
+
+        for(let j = 0; j < 6; j++){
+            let box = document.createElement('div')
+            box.className = 'clue-box'
+            box.textContent = '$' + boxValue
+            box.addEventListener('click', getClue, false)
+            row.appendChild(box)
+        }
+
+        board.appendChild(row)
+    }
+}
+
+function getClue() {
+    console.log('helllooooo')
+}
