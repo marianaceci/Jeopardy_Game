32f8476dcb3af0c25f95d21fca504282a3dd66ab
Mariana <marianaceciliaschmidt@gmail.com>
2022-08-05 17:44:13 -0300

    identify item on click

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 initBoard()
 initCatRow()
 
+document.querySelector('button').addEventListener('click', buildCategories)
+
 function initCatRow() {
     let catRow = document.getElementById('category-row')
 
@@ -36,6 +38,66 @@ function initBoard() {
     }
 }
 
-function getClue() {
-    console.log('helllooooo')
+function randInt(){
+    return Math.floor(Math.random() * (18418) + 1)
+}
+
+let catArray = []
+
+function buildCategories(){
+    const fetchReq1 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then(res => res.json());
+
+    const fetchReq2 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then(res => res.json());
+
+    const fetchReq3 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then(res => res.json());
+
+    const fetchReq4 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then(res => res.json());
+
+    const fetchReq5 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then(res => res.json());
+
+    const fetchReq6 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then(res => res.json());
+
+    const allData = Promise.all([fetchReq1, fetchReq2, fetchReq3, fetchReq4, fetchReq5, fetchReq6]);
+
+    allData.then(res => {
+        console.log(res)
+        catArray = res
+        setCategories(catArray)
+    })
+
+}
+
+//generate/load categories to the board
+function setCategories(catArray) {
+    let element = document.getElementById('category-row')
+    let children = element.children
+    for(let i = 0; i < children.length; i++){
+        children[i].innerHTML = catArray[i].title
+    }
+}
+
+//figure out which item was clicked
+function getClue(event) {
+    let child = event.currentTarget
+    child.classList.add('clicked-box')
+    let boxValue = child.innerHTML.slice(1)
+    let parent = child.parentNode
+    let index = Array.prototype.findIndex.call(parent.children, (c) => c === child)
+    let cluesList = catArray[index].clues
+    let clue = cluesList.find(obj => {
+        return obj.value == boxValue
+    })
+    showQuestion(clue, child, boxValue)
 }
